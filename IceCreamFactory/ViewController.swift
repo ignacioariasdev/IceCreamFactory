a683949739b8ed218cc1615f1e4e335507b917a6
Ignacio Arias <1992ignacio@gmail.com>
2018-12-16 10:55:49 -0800

    Initial Commit

---
+++ IceCreamFactory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IceCreamFactory
+//
+//  Created by Marlhex on 2018-12-16.
+//  Copyright © 2018 Ignacio Arias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ IceCreamFactoryTests/IceCreamFactoryTests.swift
@@ -0,0 +1,34 @@
+//
+//  IceCreamFactoryTests.swift
+//  IceCreamFactoryTests
+//
+//  Created by Marlhex on 2018-12-16.
+//  Copyright © 2018 Ignacio Arias. All rights reserved.
+//
+
+import XCTest
+@testable import IceCreamFactory
+
+class IceCreamFactoryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
